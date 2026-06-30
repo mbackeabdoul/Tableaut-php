e965fbaf937a7946dd612f8ebe9b1b7d9a7d23d3
Abdou Khoudoss Mbacke <khoudossmbacke18gmail.com>
2026-06-30 22:05:22 +0000

    initialise tableau produits

---
+++ imprative.php
@@ -0,0 +1,18 @@
+<?php
+$categories = [
+   0=> [
+        "nom" => "categorie1",
+        "code" => "006",
+        "produits" =>[
+           0=> ["nom" => "cafe","reference" => "001","quantite" => "5","prix" => "3000"],
+           1=> ["nom" => "lait","reference" => "002","quantite" => "6","prix" => "2000"]
+        ]
+    ],
+    1=> [
+        "nom" => "categorie2",
+        "code" => "007",
+        "produits" =>[]
+    ]
+];
+
+

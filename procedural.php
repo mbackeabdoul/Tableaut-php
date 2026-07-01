c6945a82e937f38ebdc779f496e00b6c933e43d8
Abdou Khoudoss Mbacke <khoudossmbacke18gmail.com>
2026-07-01 05:14:55 +0000

    saisi et cree categorie

--- procedural.php
+++ procedural.php
@@ -14,12 +14,55 @@ $categories = [
         "produits" =>[]
     ]
 ];
-function afficherCategoriesSansProduits():void{
-    global $categories;
+function afficherCategoriesSansProduits(array $categories):void{
+   
     foreach($categories as $categorie){
      if(count($categorie['produits']) === 0){
             echo $categorie['nom']."\n";
       }
     }
 }
-afficherCategoriesSansProduits();
+
+afficherCategoriesSansProduits($categories);
+
+function saisirCodeCategories(array $categories) : string {
+    $codeValide = false;
+    do {
+        $code = readline("Code:");
+        $codeValide = true;
+        foreach($categories as $categorie){
+            if($categorie['code'] == $code){
+                echo "Code deja existe!\n";
+                $codeValide = false;
+            }
+        }
+    } while($codeValide == false);
+    return $code;
+}
+
+function saisirNomCategories(array $categories) : string {
+    $nomValide = false;
+    do {
+        $nom = readline("Nom : ");
+        $nomValide = true;
+        foreach($categories as $categorie){
+            if($categorie['nom'] == $nom){
+                echo "Nom deja existant !\n";
+                $nomValide = false;
+            }
+        }
+    } while($nomValide == false);
+    return $nom;
+}
+
+function creerCategorie(array $categories) : array {
+    $code = saisirCodeCategories($categories);
+    $nom = saisirNomCategories($categories);
+    $categories[] = [
+        'nom' => $nom,
+        'code' => $code,
+        'produits' => []
+    ];
+    echo "Categorie creee !\n";
+    return $categories;
+}

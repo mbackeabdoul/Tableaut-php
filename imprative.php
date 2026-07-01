f769be8a1017d2e79bd76e981861aaa5da824b2f
Abdou Khoudoss Mbacke <khoudossmbacke18gmail.com>
2026-07-01 04:04:27 +0000

    valider code

--- imprative.php
+++ imprative.php
@@ -15,9 +15,26 @@ $categories = [
     ]
 ];
 
-foreach ($categories as $categorie) {
-    if (count($categorie['produits'])===0) {
-        echo "nom".$categorie['nom'];
+// foreach ($categories as $categorie) {
+//     if (count($categorie['produits'])===0) {
+//         echo "nom".$categorie['nom'];
+//     }
+// }
+for ($i =0; $i<count($categories); $i++){
+    if($categories[$i]['produits']===0){
+        echo "nom".$categories[$i]['nom'];
     }
 }
+
+$codeValide = false;
+do {
+    $code = readline("Code : ");
+    $codeValide = true;
+    for($i = 0; $i < count($categories); $i++){
+        if($categories[$i]['code'] == $code){
+            echo "Code deja exise!";
+            $codeValide = false;
+        }
+    }
+} while($codeValide == false);
 
